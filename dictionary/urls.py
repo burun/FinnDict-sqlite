d90c90d9acb34e69a1a335b30aa4b9a2e1fa6d01
Liang <greatliang@gmail.com>
2015-06-19 16:50:46 +0300

    Add index, word_list and word page

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from dictionary import views
 
 urlpatterns = patterns('',
-                       #url(r'^$', views.index, name='index'),
-                       #url(r'^list/$', views.list, name='list'),
-                       #url(r'^add_word/$', views.add_word, name='add_word'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'^list/$', views.list, name='list'),
+                       url(r'^word/(?P<word_name_slug>[\w\-]+)/$', views.word, name='word'),
                        )
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from dictionary.models import Word
 
-# Create your views here.
+
+def index(request):
+    context_dict = {}
+    return render(request, 'dictionary/index.html', context_dict)
+
+
+def list(request):
+        # Get the sorted vocabulary.
+    words_list = Word.objects.extra(
+        select={'case_insensitive_finnish': 'lower(finnish)'}).order_by('case_insensitive_finnish')
+    context_dict = {'words_list': words_list}
+    return render(request, 'dictionary/list.html', context_dict)
+
+
+def word(request, word_name_slug):
+    context_dict = {}
+
+    try:
+        word = Word.objects.get(slug=word_name_slug)
+        context_dict['word_finnish'] = word.finnish
+        context_dict['word_english'] = word.english
+        context_dict['word_chinese'] = word.chinese
+        context_dict['word_name_url'] = word_name_slug
+    except Word.DoesNotExist:
+        pass
+
+    return render(request, 'dictionary/word.html', context_dict)
